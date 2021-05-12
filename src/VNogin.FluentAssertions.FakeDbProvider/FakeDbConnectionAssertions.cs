db5de5bc7674bdc6f35ac32aa69a8ac3897f6dec
Vendigo82 <Vendigo82@yandex.ru>
2021-05-12 09:58:58 +0800

    Initial commit

---
+++ src/VNogin.FluentAssertions.FakeDbProvider/FakeDbConnectionAssertions.cs
@@ -0,0 +1,97 @@
+﻿using System.Linq;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using FluentAssertions.Primitives;
+using Goodwin.John.Fakes.FakeDbProvider;
+
+namespace VNogin.FluentAssertions.FakeDbProvider
+{
+    public static class FakeDbConnectionExtensions
+    {
+        public static FakeDbConnectionAssertions Should(this FakeDbConnection instance)
+        {
+            return new FakeDbConnectionAssertions(instance);
+        }
+    }
+
+    public class FakeDbConnectionAssertions : ReferenceTypeAssertions<FakeDbConnection, FakeDbConnectionAssertions>
+    {
+        public FakeDbConnectionAssertions(FakeDbConnection instance) 
+        {
+            Subject = instance;
+        }
+
+        protected override string Identifier => "FakeDbConnection";
+
+        /// <summary>
+        /// Verify DbConnection has been OpenAsync, BeginTransaction, Commit, Close and Dispose
+        /// </summary>
+        /// <returns></returns>
+        public AndWhichConstraint<FakeDbConnectionAssertions, FakeDbTransaction> HaveSingleOpenWithTransaction()
+        {
+            Execute.Assertion
+                .ForCondition(Subject.OpenAsyncCount == 1)
+                .FailWith("Expected single OpenAsync, but found {0}", Subject.OpenAsyncCount)
+                .Then
+                .ForCondition(Subject.CloseCount == 1)
+                .FailWith("Expected single Close, but found {0}", Subject.CloseCount)
+                .Then
+                .ForCondition(Subject.DisposeCount == 1)
+                .FailWith("Expected single dispose, but found {0}", Subject.DisposeCount)
+                .Then
+                .ForCondition(Subject.DbTransactions.Count == 1)
+                .FailWith("Expected single transaction, but found {0}", Subject.DbTransactions.Count)
+                .Then
+                .Given(() => Subject.DbTransactions.SingleOrDefault())
+                .ForCondition(i => i?.CommitCount == 1)
+                .FailWith("Expected single transaction with commit, but found {0}", i => i?.CommitCount);
+                
+            return new AndWhichConstraint<FakeDbConnectionAssertions, FakeDbTransaction>(this, Subject.DbTransactions.Single());
+        }
+
+        /// <summary>
+        /// Verify DbConnection has been OpenAsync, BeginTransaction, Commit, Close and Dispose
+        /// </summary>
+        /// <returns></returns>
+        public AndConstraint<FakeDbConnectionAssertions> HaveSingleOpen()
+        {
+            Execute.Assertion
+                .ForCondition(Subject.OpenAsyncCount == 1)
+                .FailWith("Expected single OpenAsync, but found {0}", Subject.OpenAsyncCount)
+                .Then
+                .ForCondition(Subject.CloseCount == 1)
+                .FailWith("Expected single Close, but found {0}", Subject.CloseCount)
+                .Then
+                .ForCondition(Subject.DisposeCount == 1)
+                .FailWith("Expected single dispose, but found {0}", Subject.DisposeCount);
+
+            return new AndConstraint<FakeDbConnectionAssertions>(this);
+        }
+
+        /// <summary>
+        /// Verify DbConnection has been OpenAsync, BeginTransaction, Commit, Close and Dispose
+        /// </summary>
+        /// <returns></returns>
+        public AndConstraint<FakeDbConnectionAssertions> HaveSingleOpenWithTransactionRollback()
+        {
+            Execute.Assertion
+                .ForCondition(Subject.OpenAsyncCount == 1)
+                .FailWith("Expected single OpenAsync, but found {0}", Subject.OpenAsyncCount)
+                .Then
+                .ForCondition(Subject.CloseCount == 1)
+                .FailWith("Expected single Close, but found {0}", Subject.CloseCount)
+                .Then
+                .ForCondition(Subject.DisposeCount == 1)
+                .FailWith("Expected single dispose, but found {0}", Subject.DisposeCount)
+                .Then
+                .ForCondition(Subject.DbTransactions.Count == 1)
+                .FailWith("Expected single transaction, but found {0}", Subject.DbTransactions.Count)
+                .Then
+                .Given(() => Subject.DbTransactions.SingleOrDefault())
+                .ForCondition(i => (i?.RollbackCount == 1 || i?.DisposeCount == 1) && i.CommitCount == 0)
+                .FailWith("Expected single transaction with rollback, but found {0}", i => i?.CommitCount);
+
+            return new AndConstraint<FakeDbConnectionAssertions>(this);
+        }
+    }
+}
---
+++ tests/VNogin.FluentAssertions.FakeDbProvider.Tests/FakeDbConnectionAssertionsTests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Threading.Tasks;
+using VNogin.FluentAssertions.FakeDbProvider;
+using Xunit;
+using Xunit.Sdk;
+using FluentAssertions;
+
+namespace Goodwin.John.Fakes.FakeDbProvider.Tests
+{
+    public class FakeDbConnectionAssertionsTests
+    {
+        readonly FakeDbConnection fakedb = new FakeDbConnection("");
+
+        [Fact]
+        public async Task HaveSingleOpenWithTransaction_Test()
+        {
+            //assert
+            await fakedb.OpenAsync();
+            var trans = await fakedb.BeginTransactionAsync();
+            await trans.CommitAsync();
+            await trans.DisposeAsync();
+            await fakedb.CloseAsync();
+            await fakedb.DisposeAsync();
+
+            //action
+            Action action = () => fakedb.Should().HaveSingleOpenWithTransaction();
+
+            // asserts
+            action.Should().NotThrow();
+        }
+
+        [Theory]
+        [InlineData(false, false, false, false, false)]
+        [InlineData(true, false, false, false, false)]
+        [InlineData(true, true, false, false, false)]
+        [InlineData(true, true, true, false, false)]
+        [InlineData(true, true, true, true, false)]
+        [InlineData(true, false, false, true, true)]
+        public async Task MustHaveSingleOpenWithTransaction_ShouldFail(
+            bool open, bool transBegin, bool commit, bool close, bool dispose)
+        {
+            //set up
+            if (open) {
+                await fakedb.OpenAsync();
+                if (transBegin) {
+                    var trans = await fakedb.BeginTransactionAsync();
+                    if (commit)
+                        await trans.CommitAsync();
+                    await trans.DisposeAsync();
+                }
+
+                if (close)
+                    await fakedb.CloseAsync();
+
+                if (dispose)
+                    await fakedb.DisposeAsync();
+            }
+
+            // action
+            Action action = () => fakedb.Should().HaveSingleOpenWithTransaction();
+
+            //assert
+            action.Should().Throw<XunitException>();
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task MustHaveSingleOpenWithTransactionRollback_ShouldSuccess(bool doRollback)
+        {
+            //assert
+            await fakedb.OpenAsync();
+            var trans = await fakedb.BeginTransactionAsync();
+            if (doRollback)
+                await trans.RollbackAsync();
+            await trans.DisposeAsync();
+            await fakedb.CloseAsync();
+            await fakedb.DisposeAsync();
+
+            //action
+            Action action = () => fakedb.Should().HaveSingleOpenWithTransactionRollback();
+
+            // asserts
+            action.Should().NotThrow();
+        }
+
+        [Theory]
+        [InlineData(false, false, false, false, false)]
+        [InlineData(true, false, false, false, false)]
+        [InlineData(true, true, false, false, false)]
+        [InlineData(true, true, true, false, false)]
+        [InlineData(true, true, true, true, false)]
+        [InlineData(true, false, false, true, true)]
+        public async Task MustHaveSingleOpenWithTransactionRollback_ShouldFail(
+            bool open, bool transBegin, bool commit, bool close, bool dispose)
+        {
+            //set up
+            if (open) {
+                await fakedb.OpenAsync();
+                if (transBegin) {
+                    var trans = await fakedb.BeginTransactionAsync();
+                    if (commit)
+                        await trans.CommitAsync();
+                    await trans.DisposeAsync();
+                }
+
+                if (close)
+                    await fakedb.CloseAsync();
+
+                if (dispose)
+                    await fakedb.DisposeAsync();
+            }
+
+            // action
+            Action action = () => fakedb.Should().HaveSingleOpenWithTransactionRollback();
+
+            //assert
+            action.Should().Throw<XunitException>();
+        }
+    }
+}
